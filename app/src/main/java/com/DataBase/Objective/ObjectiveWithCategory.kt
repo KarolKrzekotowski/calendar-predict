dc214a75b89b7e92dfed97ab238d51c56eb300f2
Aprii00 <matz027720@gmail.com>
2021-11-17 17:13:38 +0100

    Database commit v1

---
+++ app/src/main/java/com/DataBase/Objective/ObjectiveWithCategory.kt
@@ -0,0 +1,14 @@
+package com.DataBase.Objective
+
+import androidx.room.Embedded
+import androidx.room.Relation
+import com.DataBase.Category.Category
+
+data class ObjectiveWithCategory(
+    @Embedded val objective: Objective,
+    @Relation(
+        parentColumn = "category_id",
+        entityColumn = "id"
+    )
+    val category: Category
+)
