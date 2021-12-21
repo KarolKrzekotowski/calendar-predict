ec8d22afbfd69ff54e3470e91e3203ad0d3e416e
Karol_Kunicki <kunicki-karol@wp.pl>
2021-12-21 18:38:38 +0100

    prediction added(work work work work)

--- app/src/main/java/com/example/calendar_predict/objective/AgregationViewModel.kt
+++ app/src/main/java/com/example/calendar_predict/objective/AgregationViewModel.kt
@@ -1,35 +1,11 @@
 package com.example.calendar_predict.prediction
 
 import android.app.Application
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.viewModelScope
 import com.DataBase.AppDBDao
 import com.DataBase.AppDataBase
 import com.DataBase.Objective.Objective
-import com.example.calendar_predict.R
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import java.sql.Date
-import kotlin.random.Random
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
-import android.widget.AdapterView
-import android.widget.Spinner
-import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.example.calendar_predict.GoalAdapter
 import com.example.calendar_predict.GoalKind
-import com.example.calendar_predict.MygoalsDayRecyclerViewAdapter
-import kotlin.collections.ArrayList
-import com.example.calendar_predict.prediction.Tools
-import kotlinx.android.synthetic.main.statistics_page.*
 import java.util.*
 
 class AgregationViewModel(application: Application): AndroidViewModel(application) {
