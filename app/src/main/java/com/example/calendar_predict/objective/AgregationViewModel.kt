d4965739d9a7dc69cc361d44b03d3f86ba4bd51c
Karol_Kunicki <kunicki-karol@wp.pl>
2021-12-21 18:37:57 +0100

    prediction added(work work work)

--- app/src/main/java/com/example/calendar_predict/objective/AgregationViewModel.kt
+++ app/src/main/java/com/example/calendar_predict/objective/AgregationViewModel.kt
@@ -21,7 +21,6 @@ import android.graphics.drawable.ColorDrawable
 import android.widget.AdapterView
 import android.widget.Spinner
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Transformations.map
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
