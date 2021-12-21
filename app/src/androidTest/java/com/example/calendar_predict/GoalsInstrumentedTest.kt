4d4b98f1a2f0ff072bc381dae440110b797f0cbd
Kacper Wróblewski <kacper.wr00@gmail.com>
2021-12-21 12:12:01 +0100

    implementing goals and sugestion screen v1.0.2

--- app/src/androidTest/java/com/example/calendar_predict/GoalsInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/calendar_predict/GoalsInstrumentedTest.kt
@@ -1,9 +1,11 @@
 package com.example.calendar_predict
 
+import android.app.Application
 import android.view.View
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SmallTest
+import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
 import org.junit.Assert
 import org.junit.Rule
@@ -15,6 +17,27 @@ import org.junit.runner.RunWith
 class GoalsInstrumentedTest {
     @get:Rule
     val activityScenarioRule: ActivityScenarioRule<AddGoalActivity> = ActivityScenarioRule(AddGoalActivity::class.java, null)
+
+    @Test
+    fun remainingTime_isCorrect() {
+        activityScenarioRule.scenario.onActivity {
+            val testObject = GoalAdapter(it.application)
+        Assert.assertTrue("Liczba sekund do końca dnia mniejsza niż zero",
+            testObject.getSecsTillDayEnd() >= 0)
+        Assert.assertTrue("Liczba sekund do końca dnia większa niż 1 dzień",
+            testObject.getSecsTillDayEnd() < 24 * 60 * 60)
+
+        Assert.assertTrue("Liczba dni do końca tygodnia mniejsza niż zero",
+            testObject.getDaysTillWeekEnd() >= 0)
+        Assert.assertTrue("Liczba dni do końca tygodnia większa niż 1 dzień",
+            testObject.getDaysTillWeekEnd() < 7)
+
+        Assert.assertTrue("Liczba dni do końca miesiąca mniejsza niż zero",
+            testObject.getDaysTillMonthEnd() >= 0)
+        Assert.assertTrue("Liczba dni do końca miesięca większa niż 30",
+            testObject.getDaysTillMonthEnd() < 30)
+        }
+    }
 
     @Test
     fun selectedTimeAmount_isCorrect() {
--- app/src/test/java/com/example/calendar_predict/GoalsTest.kt
+++ app/src/test/java/com/example/calendar_predict/GoalsTest.kt
@@ -7,22 +7,12 @@ import org.junit.Assert
 import org.junit.Test
 
 class GoalsTest {
+//    placeholder test to suggest syntax
     @Test
     fun remainingTime_isCorrect() {
-        val testObject = GoalAdapter()
         Assert.assertTrue("Liczba sekund do końca dnia mniejsza niż zero",
-            testObject.getSecsTillDayEnd() >= 0)
-        Assert.assertTrue("Liczba sekund do końca dnia większa niż 1 dzień",
-            testObject.getSecsTillDayEnd() < 24 * 60 * 60)
-
-        Assert.assertTrue("Liczba dni do końca tygodnia mniejsza niż zero",
-            testObject.getDaysTillWeekEnd() >= 0)
-        Assert.assertTrue("Liczba dni do końca tygodnia większa niż 1 dzień",
-            testObject.getDaysTillWeekEnd() < 7)
-
-        Assert.assertTrue("Liczba dni do końca miesiąca mniejsza niż zero",
-            testObject.getDaysTillMonthEnd() >= 0)
-        Assert.assertTrue("Liczba dni do końca miesięca większa niż 30",
-            testObject.getDaysTillMonthEnd() < 30)
+            true)
+        Assert.assertFalse("Liczba sekund do końca dnia większa niż 1 dzień",
+            false)
     }
 }
